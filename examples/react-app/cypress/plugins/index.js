b8fe355f0229dc0f7aa9b0bb64ec12538ff851e8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-10 13:43:52 -0400

    delete webpack optimization

--- examples/react-app/cypress/plugins/index.js
+++ examples/react-app/cypress/plugins/index.js
@@ -1,12 +1,24 @@
 const findWebpack = require('find-webpack')
 const webpackPreprocessor = require('../../../..')
 
-module.exports = (on, config) => {
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on) => {
+  // find the Webpack config used by react-scripts
   const webpackOptions = findWebpack.getWebpackOptions()
 
   if (!webpackOptions) {
     throw new Error('Could not find Webpack in this project 😢')
   }
+
+  // if we just pass webpackOptions to the preprocessor
+  // it won't work - because react-scripts by default
+  // includes plugins that split specs into chunks, etc.
+  // https://github.com/cypress-io/cypress-webpack-preprocessor/issues/31
+
+  // solution 1
+  delete webpackOptions.optimization
 
   const options = {
     webpackOptions,
