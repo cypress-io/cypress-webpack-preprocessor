87e2bc2c6a1b4e52c14329854ec9ae37b59df752
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-14 14:39:02 -0400

    try solving typecheck

--- examples/use-ts-loader/cypress/integration/spec.ts
+++ examples/use-ts-loader/cypress/integration/spec.ts
@@ -8,5 +8,10 @@ describe('Use .babelrc', () => {
     }
 
     expect(data.person.firstName).to.equal('Joe')
+
+    cy.wrap(data).should('have.property', 'person')
+    .should('deep.equal', {
+      firstName: 'Joe',
+    })
   })
 })
